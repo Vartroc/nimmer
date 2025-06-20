5b27935bc50084e40651d5d52bb4cbb88b1e5ed5
Andreas <andi@thurau.net>
2025-06-20 17:54:13 +0200

    set to stable for deno to build, fix later | flake.nix

--- flake.nix
+++ flake.nix
@@ -1,31 +1,38 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: {
-    packages."x86_64-linux" = let
-        neovimConfigured = (inputs.nvf.lib.neovimConfiguration {
-          inherit (nixpkgs.legacyPackages."x86_64-linux") pkgs;
-          modules = [
-	    ./default.nix   
-	  ];
-	  extraSpecialArgs = {
-	    # Inputs is seemingly reserved, though the docs don't show it - LilleAila nvf-config
-	    inputs' = inputs;
-	  };
-        });
-    in {
-      # Set the default package to the wrapped instance of Neovim.
-      # This will allow running your Neovim configuration with
-      # `nix run` and in addition, sharing your configuration with
-      # other users in case your repository is public.
-      default = neovimConfigured.neovim;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    {
+      packages."x86_64-linux" =
+        let
+          neovimConfigured = (
+            inputs.nvf.lib.neovimConfiguration {
+              inherit (nixpkgs.legacyPackages."x86_64-linux") pkgs;
+              modules = [
+                ./default.nix
+              ];
+              extraSpecialArgs = {
+                # Inputs is seemingly reserved, though the docs don't show it - LilleAila nvf-config
+                inputs' = inputs;
+              };
+            }
+          );
+        in
+        {
+          # Set the default package to the wrapped instance of Neovim.
+          # This will allow running your Neovim configuration with
+          # `nix run` and in addition, sharing your configuration with
+          # other users in case your repository is public.
+          default = neovimConfigured.neovim;
+        };
     };
-  };
 }
